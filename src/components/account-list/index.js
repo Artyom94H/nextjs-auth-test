5b3f8d3515c0845296a180083fac8f0edbfe96e2
Artyom <harutyunyanartyom94@gmail.com>
2023-01-30 01:44:57 +0400

    Add loader

--- src/components/account-list/index.js
+++ src/components/account-list/index.js
@@ -1,47 +1,77 @@
-import { Col, Row, Typography, Avatar } from 'antd';
+import { Col, Row, Typography, Avatar, Skeleton } from 'antd';
 import { StyledAccountItem } from 'components/account-list/styled';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
+import { useMemo } from 'react';
 
-const AccountItem = ({ data = {} }) => {
+const LoaderItem = () => {
   return (
     <>
       <StyledAccountItem span={24}>
-       <Link to={ROUTES.accountsGuest.path.replace(':id', data.slug)}>
-         <Row align='middle'>
-           <Col xxl={1} xl={1} lg={1} md={1} xs={4} sm={4}>
-             <Avatar />
-           </Col>
-           <Col xxl={23} xl={23} lg={23} md={23} xs={20} sm={20}>
-             <Row justify='space-between'>
-               <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
-                 {data.name}
-               </Col>
-               <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
-                 {data.email}
-               </Col>
-             </Row>
-           </Col>
-         </Row>
-       </Link>
+        <Row align='middle'>
+          <Col xxl={1} xl={1} lg={1} md={1} xs={4} sm={4}>
+            <Skeleton.Avatar active={true} />
+          </Col>
+          <Col xxl={23} xl={23} lg={23} md={23} xs={20} sm={20}>
+            <Row justify='space-between'>
+              <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
+                <Skeleton.Input active={true} />
+              </Col>
+              <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
+                <Skeleton.Input active={true} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </StyledAccountItem>
     </>
   );
 };
 
-const AccountList = ({ data = [] }) => {
+const AccountItem = ({ data = {} }) => {
   return (
     <>
-      <Typography.Title>
-        Список аккаунтов
-      </Typography.Title>
+      <StyledAccountItem span={24}>
+        <Link to={ROUTES.accountsGuest.path.replace(':id', data.slug)}>
+          <Row align='middle'>
+            <Col xxl={1} xl={1} lg={1} md={1} xs={4} sm={4}>
+              <Avatar />
+            </Col>
+            <Col xxl={23} xl={23} lg={23} md={23} xs={20} sm={20}>
+              <Row justify='space-between'>
+                <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
+                  {data.name}
+                </Col>
+                <Col xxl={12} xl={12} lg={12} md={12} xs={24} sm={24}>
+                  {data.email}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Link>
+      </StyledAccountItem>
+    </>
+  );
+};
+
+const AccountList = ({ loading, data = [] }) => {
+  const arrForLoader = useMemo(() => {
+    return Array.from(Array(10).keys());
+  }, []);
+
+  return (
+    <>
+      <Typography.Title>Список аккаунтов</Typography.Title>
       <Row gutter={[]}>
-        {/*<Col span={24}>*/}
-        {/*  <Divider />*/}
-        {/*</Col>*/}
-        {data?.map((i, index) => (
-          <AccountItem key={`${i.email}_${index}`} data={i} />
-        ))}
+        {!!loading ? (
+          arrForLoader.map((i, index) => <LoaderItem key={index.toString()} />)
+        ) : (
+          <>
+            {data?.map((i, index) => (
+              <AccountItem key={`${i.email}_${index}`} data={i} />
+            ))}
+          </>
+        )}
       </Row>
     </>
   );
--- src/components/account/index.js
+++ src/components/account/index.js
@@ -2,40 +2,48 @@ import { StyledAccountHero } from 'components/account/styled';
 import { Button, Col, Row, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
+import Container from 'components/container';
 
 const Account = ({ edit = false, user = {} }) => {
   return (
     <>
       <StyledAccountHero />
-      <Row gutter={[0, 12]}>
-        <Col span={24}>
-          <Row>
-            <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
-              <Typography.Title>
-                {user?.name}
-              </Typography.Title>
-              <Typography.Title level={5} type='secondary'>
-                {user?.email}
-              </Typography.Title>
-            </Col>
-            <Col xxl={6} xl={6} lg={6} md={6} sm={24} xs={24}>
-              <Link to={ROUTES.accountsOwnerEdit.path}>
-                <Button>
-                  Edit
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-        </Col>
-        <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
+      <Container>
+        <Row gutter={[0, 12]}>
+          <Col span={24}>
+            <Row>
+              <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
+                <Typography.Title>
+                  {user?.name}
+                </Typography.Title>
+                <Typography.Title level={5} type='secondary'>
+                  {user?.email}
+                </Typography.Title>
+              </Col>
+              {!!edit && (
+                <Col xxl={6} xl={6} lg={6} md={6} sm={24} xs={24}>
+                  <Link to={ROUTES.accountsOwnerEdit.path}>
+                    <Button>
+                      Edit
+                    </Button>
+                  </Link>
+                </Col>
+              )}
+            </Row>
+          </Col>
+          <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
 
-        </Col>
-        <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
-          <Button>
-            Sign out
-          </Button>
-        </Col>
-      </Row>
+          </Col>
+          {!!edit && (
+            <Col xxl={18} xl={18} lg={18} md={18} sm={24} xs={24}>
+              <Button>
+                Sign out
+              </Button>
+            </Col>
+          )}
+
+        </Row>
+      </Container>
     </>
   );
 };
